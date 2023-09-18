b47f66a928a3ffece468d75f400eaebf355000ef
Pooja <poojadeepakchennoria@gmail.com>
2023-09-18 21:30:55 +0530

    Update

--- POMSeleniumProject/src/test/java/Tests/TestBase/TestCasesBase.java
+++ POMSeleniumProject/src/test/java/Tests/TestBase/TestCasesBase.java
@@ -9,19 +9,35 @@ import Utils.ReadJasonUtility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestCasesBase {
-	
+
 	public WebDriver driver;
 
 	@BeforeTest
 	public void SetUp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		switch (ReadJasonUtility.GetValueFromConfig("Browser")) {
+
+		case "Chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+
+		case "IE":
+			break;
+
+		case "Firefox":
+			break;
+
+		default:
+			System.out.println("Wrong browser");
+		}
+
 		driver.manage().window().maximize();
 		driver.get(ReadJasonUtility.GetValueFromConfig("AppURL"));
 	}
 
 	@AfterTest
 	public void TearDown() {
+		//driver.close();
 		driver.quit();
 	}
 
